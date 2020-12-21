c92fc11ef5bd57acb7d7f9741fedf03e3068e5a5
Jennifer Ross <jjannross@gmail.com>
2020-12-21 14:44:45 -0500

    added main structure, files, and folders

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// series of questions:
+// if correct then good sound and next question 
+
+// if incorrect, bad sound and time subtracted 
+
+// game over when all questions are answered or time reaches 0
+
+// field to input user name
+// scores displayed 
